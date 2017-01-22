dcabde5f46a662f9757eef53f94cf9111ab4cc82
MaksKolomiiets <kolomiiets.maks@gmail.com>
2017-01-22 19:21:08 +0200

    add new project

---
+++ js/script.js
@@ -0,0 +1,70 @@
+/* ---------------------------------------------- /*
+ * Preloader
+/* ---------------------------------------------- */
+
+$(window).load(function() {
+		$('#status').fadeOut();
+		$('#preloader').delay(300).fadeOut('slow');
+});
+
+
+$(document).ready(function () {
+
+
+	/* ---------------------------------------------- /*
+	* Size Header BG
+	/* ---------------------------------------------- */
+
+	$(".bg-screen-height").height($(window).height());
+
+	$(window).resize(function(){
+		$(".bg-screen-height").height($(window).height());
+	});
+
+
+	/* ---------------------------------------------- /*
+	* Smooth scroll
+	/* ---------------------------------------------- */
+
+	$("a[href^='#']").bind("click", function(e){
+		e.preventDefault();
+		var anchor = $(this);
+
+		$('html, body').stop().animate({
+			scrollTop: $(anchor.attr('href')).offset().top
+		}, 1000);
+	});
+
+	$(window).scroll(function() {
+		var elems = $("section");
+		var coord = $(document).scrollTop();
+
+		if(coord == elems.offset().top - 300) {
+			$(".one").animate({
+				opacity: '1'
+			},500);
+		};
+	});
+
+
+	/* ---------------------------------------------- /*
+	*   Form effect
+	/* ---------------------------------------------- */
+
+	$("#contact-form input, #contact-form textarea").on("focusin", function() {
+        $(this).prev().css("marginTop", "-35px")
+    }).on("focusout", function() {
+        return !$(this).val() && void $(this).prev().css("marginTop", "0px")
+    });
+
+
+	/* ---------------------------------------------- /*
+	 * WOW Animation When You Scroll
+	/* ---------------------------------------------- */
+
+    wow = new WOW({
+			mobile: false
+		});
+		wow.init();
+
+});
